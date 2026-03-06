a67db209874fb40a5e3771d15ba1c37b23638c31
Chris Stone <chris@stoneyforest.net>
2026-03-06 13:45:52 -0500

    Initial

---
+++ brands_gen.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use v5.10;
+
+/**
+ * @file brands_gen.pl
+ * @author Chris Stone
+ * @version 1.1.3
+ * @description Generates SpamAssassin rules for Brands, Subjects, and Brand+Lure combinations.
+ */
+
+# --- Configuration ---
+my $input_brands = 'brands.txt';
+my $input_lures  = 'lures.txt';
+my $output_file  = 'local_brands.cf';
+my $spam_score   = '5.0';
+my $lure_score   = '2.5'; # Additional score for Brand+Lure matches
+my $rule_prefix  = 'LOCAL_SPAM_WK_';
+# ---
+
+open(my $br_fh, '<', $input_brands) or die "Error: Cannot open '$input_brands': $!\n";
+open(my $out_fh, '>', $output_file) or die "Error: Cannot open '$output_file': $!\n";
+
+# Load Lures into an array for cross-referencing
+my @lures;
+if (-e $input_lures) {
+    open(my $lu_fh, '<', $input_lures);
+    while (<$lu_fh>) {
+        chomp;
+        next if /^\s*(#.*)?$/;
+        push @lures, $_;
+    }
+    close($lu_fh);
+}
+
+say "Processing brands and lures to generate '$output_file'...";
+
+# --- Generate Global Lure Rules First ---
+foreach my $lure (@lures) {
+    my $l_id = uc($lure);
+    $l_id =~ s/[^A-Z0-9]//g;
+    printf $out_fh "header __%sLURE_%s\tSubject =~ /\\b%s\\b/i\n", $rule_prefix, $l_id, $lure;
+}
+print $out_fh "\n";
+
+# --- Process Brands ---
+while (my $brand_regex = <$br_fh>) {
+    chomp $brand_regex;
+    next if $brand_regex =~ /^\s*(#.*)?$/;
+
+    my $identifier = $brand_regex;
+    $identifier =~ s/\\W\?//g; 
+    $identifier =~ s/\.\?//g;  
+    $identifier =~ s/\\s\*//g; 
+    $identifier =~ s/[^a-zA-Z0-9]+//g;
+    $identifier = uc($identifier);
+
+    if (length($identifier) == 0) { next; }
+
+    # Standard Brand Rules (Name, Addr, Subject)
+    printf $out_fh "# Rules for: %s\n", $identifier;
+    printf $out_fh "header __%s%sa\tFrom:name =~ /\\b%s\\b/i\n", $rule_prefix, $identifier, $brand_regex;
+    printf $out_fh "header __%s%sb\tFrom:addr =~ /%s\\./\n", $rule_prefix, $identifier, $brand_regex;
+    printf $out_fh "header __%s%ss\tSubject =~ /\\b%s\\b/i\n", $rule_prefix, $identifier, $brand_regex;
+
+    # Standard Meta Rules
+    printf $out_fh "meta   %s%s\t(__%s%sa && !__%s%sb)\n", $rule_prefix, $identifier, $rule_prefix, $identifier, $rule_prefix, $identifier;
+    printf $out_fh "score  %s%s\t%s\n", $rule_prefix, $identifier, $spam_score;
+
+    printf $out_fh "meta   %s%ss\t(__%s%ss && !__%s%sb)\n", $rule_prefix, $identifier, $rule_prefix, $identifier, $rule_prefix, $identifier;
+    printf $out_fh "score  %s%ss\t%s\n", $rule_prefix, $identifier, $spam_score;
+
+    # Brand + Lure Logic
+    foreach my $lure (@lures) {
+        my $l_id = uc($lure);
+        $l_id =~ s/[^A-Z0-9]//g;
+        
+        # Trigger if (Brand in Name OR Brand in Subject) AND (Lure in Subject) AND (Not in Addr)
+        printf $out_fh "meta   %s%s_%s\t((__%s%sa || __%s%ss) && __%sLURE_%s && !__%s%sb)\n",
+            $rule_prefix, $identifier, $l_id, $rule_prefix, $identifier, $rule_prefix, $identifier, $rule_prefix, $l_id, $rule_prefix, $identifier;
+        printf $out_fh "score  %s%s_%s\t%s\n", $rule_prefix, $identifier, $l_id, $lure_score;
+    }
+
+    print $out_fh "\n";
+}
+
+close($br_fh);
+close($out_fh);
+
+say "Complete. Version 1.1.3 generated.";
